47b0f998bc1dedd96cf439eefe0fe303cb92657b
Hayden Goldstien <haydengoldstien@gmail.com>
2014-11-24 01:43:25 -0600

    Spectrum cleaning complete (Hashmap -> ArrayList)

--- src/Spectrum.java
+++ src/Spectrum.java
@@ -5,22 +5,22 @@ import java.util.*;
  */
 public class Spectrum {
     public static class Graph {
-        private HashMap<String, Integer> vertices;
+        private ArrayList<String> vertices;
         private HashMap<String, ArrayList<String>> adj;
         public void init(){
-            vertices = new HashMap<String, Integer>();
+            vertices = new ArrayList<String>();
             adj = new HashMap<String, ArrayList<String>>();
         }
         public void addVertex(String vertex) {
-            if (!vertices.containsKey(vertex)) {
-                vertices.put(vertex, vertices.size());
+            if (!vertices.contains(vertex)) {
+                vertices.add(vertex);
                 adj.put(vertex, new ArrayList<String>());
             }
         }
 
         public void addEdge(String vertex1, String vertex2) {
-            if (!vertices.containsKey(vertex1)) addVertex(vertex1);
-            if (!vertices.containsKey(vertex2)) addVertex(vertex2);
+            if (!vertices.contains(vertex1)) addVertex(vertex1);
+            if (!vertices.contains(vertex2)) addVertex(vertex2);
             if (!(vertex1.equals(vertex2)) && (!adj.get(vertex1).contains(vertex2))) {
                 adj.get(vertex1).add(vertex2);
                 adj.get(vertex2).add(vertex1);
@@ -28,13 +28,13 @@ public class Spectrum {
         }
 
         public boolean contains(String vertex) {
-            return vertices.containsKey(vertex);
+            return vertices.contains(vertex);
         }
 
         public HashMap<String, Integer> bredthFirstSearch(String sourceVertex)
         {
             HashMap<String, Integer> distances = new HashMap<String, Integer>();
-            for (String vertex : vertices.keySet())
+            for (String vertex : vertices)
             {
                 distances.put(vertex, 100000);
             }
@@ -57,7 +57,6 @@ public class Spectrum {
 
         public HashMap<Integer, Integer> connections(String from)
         {
-            Integer vertex = vertices.get(from);
             if (adj.get(from).size() == 0){
                 return new HashMap<Integer, Integer>();
             }
@@ -79,8 +78,6 @@ public class Spectrum {
 
         public int hops(String vertex1, String vertex2)
         {
-            Integer v1 = vertices.get(vertex1);
-            Integer v2 = vertices.get(vertex2);
             HashMap<String,Integer> mapping = bredthFirstSearch(vertex1); //THIS IS WRONG
             if (mapping.containsKey(vertex2)){
                 if (mapping.get(vertex2) == 100000){
@@ -143,7 +140,7 @@ public class Spectrum {
                     }
                 }
             }
-            if (split[0] == "reset") {
+            if (split[0].equals("reset")) {
                 System.out.println("----------");
                 caseNum++;
                 graph = new Graph();
